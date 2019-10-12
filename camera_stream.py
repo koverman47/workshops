676d06c854bc715b678a9510569c3eb92fc8673b
Kirby <knoverman@comcast.net>
2019-10-11 21:13:53 -0700

    cam stream

---
+++ camera_stream.py
@@ -0,0 +1,32 @@
+from picamera.array import PiYUVArray, PiRGBArray
+from picamera import PiCamera
+
+from scipy.signal import find_peaks, butter, filtfilt
+
+import time
+import matplotlib.pyplot as plt
+import skimage as ski
+
+res = (640, 480)
+
+camera = PiCamera()
+
+# Check the link below for the combinations between mode and resolution
+# https://picamera.readthedocs.io/en/release-1.13/fov.html#sensor-modes
+camera.sensor_mode = 7
+camera.resolution = res
+camera.framerate = 30
+
+# Initialize the buffer and start capturing
+rawCapture = PiRGBArray(camera, size=res)
+stream = camera.capture_continuous(rawCapture, format="rgb", use_video_port=True)
+
+frame = next(stream)
+
+for f in frame:
+    rawCapture.truncate(0)
+    plt.imshow(frame.array)
+
+stream.close()
+rawCapture.close()
+camera.close()
